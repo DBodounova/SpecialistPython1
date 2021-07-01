a2839feccf90ab1be4f843b82493855f70168acd
DBodounova <86655117+DBodounova@users.noreply.github.com>
2021-07-01 15:20:24 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -9,3 +9,16 @@ def average(*args):
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
 print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+
+
+# Напишите функцию, находящую среднее арифметическое всех аргументов
+# Функция должна вызываться с любым количеством аргументов
+from itertools import count
+
+
+def average(*args):
+    return  sum(args)/(len(args))
+
+print(average(3, 4, 8))
+print(average(1, 4, 5, -3, 8, 4))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2))
